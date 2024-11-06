786da1f54eb4bad02e43f1ebad6e3aef5365fca1
Sky Song <hs359@byu.edu>
2024-11-06 19:24:47 +0000

    changed everything for milestone3

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
 let express = require("express");
-let app = new express();                                                                     
-app.set("view engine","ejs")
+let app = express();
+app.set("view engine", "ejs");
 
 // set up database connection
 const knex = require("knex")({
- client: "mysql",
- connection: {
-  host:"concert-db.cp4e2g68854m.us-east-1.rds.amazonaws.com",
-  user: "admin",
-  password: "Utah2021!",
-  database:"paradise-concerts",
-  port: 3306,
- },
+  client: "mysql",
+  connection: {
+    host: "concert-db.cp4e2g68854m.us-east-1.rds.amazonaws.com",
+    user: "admin",
+    password: "Utah2021!",
+    database: "paradise-concerts",
+    port: 3306,
+  },
 });
 
-app.get("/",(req,res) => {
- knex
- .select()
- .from("venues")
- .then((result) => {
-  res.render("index", {aConcerts: result});
- }); 
+app.get("/", (req, res) => {
+  knex
+    .select()
+    .from("venues")
+    .then((result) => {
+      res.render("index", { movies: result }); // Pass data as 'movies'
+    })
+    .catch((error) => {
+      console.error("Database query error:", error);
+      res.status(500).send("Error retrieving data");
+    });
 });
-app.listen(3000);
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
 
